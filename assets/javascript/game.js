535656e5829e1595798fdc9e40bc1d016121928a
TeresaG <tmg.coder@gmail.com>
2019-04-16 10:06:53 -0700

    pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Create computerChoice array with all of the letter options in capitals
+
+// Create computerGuess var.  Make sure it is outside, so as not to reset each time the user selects
+
+// Create guessesSoFar array for user guesses to be pushed to
+
+// Create wins/losses/guessesLeft var
+
+//onKeyUp function
+
+// 
