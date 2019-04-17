b5970b0afa8cede71cc265703475d5ee5bd1e62c
TeresaG <tmg.coder@gmail.com>
2019-04-17 14:06:12 -0700

    js start

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,9 @@
 //onKeyUp function
 
 // 
+var computerChoice = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];//
+var guessesSoFar = [];//Array to push user choices to
+var wins = 0;
+var losses = 0;
+guessesLeft = 9;
